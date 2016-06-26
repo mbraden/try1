962ccb10409cc33809df0022efd0916f7d937907
mike <mike@LT-0514>
2016-06-26 17:40:21 -0500

    Add project files.

---
+++ try1/try1/Core.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try1
+{
+    class Core
+    {
+
+
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            //Sign up for a free API key at http://openweathermap.org/appid
+            string key = "0c471aaf8d6a350dad9634c7a4820c17";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
+                + zipCode + ",us&appid=" + key + "&units=imperial";
+
+            dynamic results = await DataServices.getDataFromService(queryString).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+
+
+    }
+}
